2866b8d9714c8707b1c7c7db4605c805bdc18824
Pablo-D-P-C <pablodanielperalta@hotmail.com>
2020-04-27 12:55:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Bienvenido a FizzBuzz!!!")
+num = int(input("Elige un número del 1 al 100: "))
+
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
